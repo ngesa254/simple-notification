cf3273f621ca3e7a28289de3ce6c952420f1fb1f
ngesa marvin <marvinngesa@gmail.com>
2018-02-09 03:00:36 +0300

    declare variables for Button, NotificationManager, Notification and PendingIntent

--- app/src/main/java/io/droidmarvin/simplenotification/MainActivity.java
+++ app/src/main/java/io/droidmarvin/simplenotification/MainActivity.java
@@ -1,13 +1,24 @@
 package io.droidmarvin.simplenotification;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonNotificationShow, buttonNotificationClear;
+    NotificationManager notificationManager;
+    Notification mNotification;
+    PendingIntent mPendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
